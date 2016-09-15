92080b47feae3d3a3426b790d8b5aecd786fac35
Mateus Guimarães <mateus.jatene@gmail.com>
2016-09-15 19:43:36 -0300

    Minimal command test

---
+++ tests/Commands/MakeTransformerCommandTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Mateusjatenee\LaravelArtisanExtended\Commands\MakeTransformerCommand;
+use Mockery as M;
+
+class MakeTransformerCommmandTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        $this->filesystem = M::mock(Illuminate\Filesystem\Filesystem::class);
+        $this->input = M::mock(Symfony\Component\Console\Input\InputInterface::class);
+    }
+
+    public function tearDown()
+    {
+        M::close();
+    }
+
+    /** @test */
+    public function it_guesses_the_model()
+    {
+        $command = new MakeTransformerCommand($this->filesystem);
+        $this->assertEquals('App\User', $command->getModel('App/User'));
+        $this->assertEquals('App\User', $command->getModel('App\User'));
+    }
+}
